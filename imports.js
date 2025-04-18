0ecfd65f633acc4f40cbdb5061b583501017d927
Chris Hargis <chargis86@gmail.com>
2025-04-18 15:04:06 -0400

    remove of type=module and import statement

--- imports.js
+++ imports.js
@@ -1,6 +1,7 @@
 //imports
 
-import { createGrid, AllCommunityModule, ModuleRegistry, themeQuartz} from 'ag-grid-community';
+// Access ag-grid-community APIs from the global agGrid object
+const { createGrid, AllCommunityModule, ModuleRegistry, themeQuartz } = window.agGrid;
 
 // Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
